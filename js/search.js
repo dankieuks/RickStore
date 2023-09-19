049a7097d80bb09ea301f381849adbc4f47dbd72
dankieu <kieudinhdan02@gmail.com>
2023-09-19 15:30:06 +0700

    hh

--- js/search.js
+++
@@ -1,6 +0,0 @@
-let searchProduct=document.getElementsByClassName("search-products");
-const BASE_URL = 'https://636a61ecc07d8f936d9c3114.mockapi.io/Final';
-for(let i =0;i<searchProduct.length; i++){
-    BASE_URL.includes("Ankle Boots")
-}
-BASE_URL.includes("Ankle Boots")
